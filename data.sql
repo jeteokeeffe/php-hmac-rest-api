9ad6f97fc3c2e7bffbd6bea8457610b80c1eb9fd
Jete O'Keeffe <jeteokeeffe@yahoo.com>
2013-08-26 12:04:21 -0400

    added unique key for private key, again, more documentation about the api

--- data.sql
+++ data.sql
@@ -1,8 +1,11 @@
 CREATE TABLE `api` (
   `client_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `public_id` char(64) NOT NULL DEFAULT '',
   `private_key` char(64) NOT NULL DEFAULT '',
   `status` enum('ACTIVE','INACTIVE') DEFAULT 'ACTIVE',
-  PRIMARY KEY (`client_id`)
+  PRIMARY KEY (`client_id`),
+  UNIQUE KEY `private_key` (`private_key`),
+  UNIQUE KEY `public_id` (`public_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 INSERT INTO api SET private_key = '593fe6ed77014f9507761028801aa376f141916bd26b1b3f0271b5ec3135b989';
